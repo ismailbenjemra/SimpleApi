7315785a1a31674dbc3d2c6809a0f89d0774ba12
ISMAIL BENJEMRA <ismailbenjemra@gmail.com>
2021-10-31 22:38:01 +0100

    first build add

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -29,6 +29,7 @@ namespace SimpleAPI.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+                //test first build
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
